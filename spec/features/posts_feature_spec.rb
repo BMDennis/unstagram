bab63398f16e711e2005114cfb524f9a99dfc359
apostolistax <a.taxidaridis@gmail.com>
2014-05-28 12:10:54 +0100

    Adding tests for posts

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,37 +1,40 @@
 require 'spec_helper'
 
-describe 'posts' do
-  context 'no posts' do
-    it 'displays a prompt to create posts' do
-      visit '/posts'
+  describe 'posts' do
+    context 'no posts' do
+      it 'displays a message' do
+        visit '/posts'
 
-      expect(page).to have_content 'No posts yet'
-      expect(page).to have_link 'New post'
+        expect(page).to have_content "No posts"
+        expect(page).to have_content "New post"
+      end
+    end
+
+    context 'with posts' do
+      it 'displays the posts' do
+        visit '/posts'
+
+        Post.create(title: 'Cool post', description: 'Hello world')
+        expect(page).to have_content 'Cool post'
+      end
     end
   end
 
-  context 'with posts' do
-    before { Post.create(title: 'Cool post', description: 'Hello world') }
+  describe 'adding posts' do
+    it 'adds the post to the homepage' do
+      visit '/posts/new'
+      fill_in 'Title', with: 'My new post'
+      fill_in 'Description', with: 'description'
+      attach_file 'Picture', Rails.root.join('spec/images/old-man1.jpg')
 
-    it 'displays the posts' do
-      visit '/posts'
+      click_button 'Post it!'
 
-      expect(page).to have_content 'Cool post'
+      expect(current_path).to eq posts_path
+      expect(page).to have_content 'My new post'
+      expect(page).to have_content 'img.uploaded-pic'
     end
   end
-end
 
-describe 'creating posts' do
-  it 'adds the post to the homepage' do
-    visit '/posts/new'
-    fill_in 'Title', with: 'My new post'
-    fill_in 'Description', with: 'Lorem ipsum'
-    attach_file 'Picture', Rails.root.join('spec/images/old-man1.jpg')
 
-    click_button 'Post it!'
 
-    expect(current_path).to eq posts_path
-    expect(page).to have_content 'My new post'
-    expect(page).to have_css 'img.uploaded-pic'
-  end
-end
+
