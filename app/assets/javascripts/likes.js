1c7bc02cd688a10eb1ecf1aee244ac1b4d86a487
apostolistax <a.taxidaridis@gmail.com>
2014-06-05 18:26:03 +0100

    css tweaks

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -5,13 +5,11 @@
 $(document).ready(function () {
   $('.btn-like').on('click', function(event){
     event.preventDefault();
-
     $.post(this.href);
   });
 
   var connection = new WebSocketRails(window.location.host + '/websocket');
   channel = connection.subscribe('likes');
-
   channel.bind('new', function(post) {
     var postElem = $('.post[data-id=' + post.id + '] .btn-like')
     postElem.text('❤ ' + post.new_like_count);
